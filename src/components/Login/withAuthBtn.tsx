da239b64f646e3afce4d08086c0c7783214fd0d2
Art <artgurianov@yandex.ru>
2025-05-09 20:25:04 +0300

    fix

--- src/components/Login/withAuthBtn.tsx
+++ src/components/Login/withAuthBtn.tsx
@@ -9,7 +9,7 @@ import { useSession } from "next-auth/react";
 export const withAuthBtn = <T extends GetComponentProps<typeof Button>>(
   component: FC<T>
 ): FC<T> => {
-  return (props: T) => {
+  const Comp = (props: T) => {
     const { data, status } = useSession();
     const Cmp = component;
     Cmp.displayName = "Button";
@@ -26,4 +26,7 @@ export const withAuthBtn = <T extends GetComponentProps<typeof Button>>(
       </GuardedLoginBtn>
     );
   };
+  Comp.displayName = "WithAuthBtn";
+
+  return Comp;
 };

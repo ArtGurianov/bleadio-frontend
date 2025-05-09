332698eabb9526dd17cb58a1e4bba93458fa16bf
Art <artgurianov@yandex.ru>
2025-05-09 15:25:38 +0300

    fix

--- src/components/Motto/Motto.tsx
+++ src/components/Motto/Motto.tsx
@@ -4,15 +4,15 @@ export const Motto = () => {
       <div className="w-full bg-accent/50 border-l-4 border-b-6 border-primary p-2 text-center rounded-md rounded-tl-none">
         <span className="font-mono text-wrap text-xl">
           {"Receive custom "}
-          <span className="text-2xl">{"notifications"}</span>
+          <span className="text-2xl text-primary">{"notifications"}</span>
           <span>{" from your e-businesses"}</span>
           <br />
           {"straight to your "}
-          <span className="text-2xl">{"personal Telegram."}</span>
+          <span className="text-2xl text-primary">{"personal Telegram."}</span>
           <br />
-          <span className="text-2xl">{"Easy"}</span>
+          <span className="text-2xl text-primary">{"Easy"}</span>
           {" integration. "}
-          <span className="text-2xl">{"Free plan"}</span>
+          <span className="text-2xl text-primary">{"Free plan"}</span>
           {" available."}
         </span>
       </div>

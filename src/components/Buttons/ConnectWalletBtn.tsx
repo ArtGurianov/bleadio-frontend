066e132bea3efa56a1e45e56938a9b73e69b6501
Art <artgurianov@yandex.ru>
2025-05-11 14:32:43 +0300

    metamask onboarding

--- src/components/Buttons/ConnectWalletBtn.tsx
+++ src/components/Buttons/ConnectWalletBtn.tsx
@@ -1,26 +1,31 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { useAccount, useConnect } from "wagmi";
 
 export const ConnectWalletBtn = () => {
   const { isConnected } = useAccount();
   const { connect, connectors } = useConnect();
+  const connectorIndex = connectors.findIndex(
+    (connector) => connector.name === "MetaMask"
+  );
 
-  return !isConnected ? (
-    <>
-      {connectors.map((connector) =>
-        connector.name !== "Injected" ? (
-          <Button
-            key={connector.uid}
-            onClick={() => {
-              connect({ connector });
-            }}
-          >
-            {`Connect ${connector.name}`}
-          </Button>
-        ) : null
+  const handleClick = () => {
+    if (connectorIndex !== -1) {
+      connect({ connector: connectors[connectorIndex] });
+    }
+  };
+
+  if (isConnected) return null;
+
+  return (
+    <Button onClick={handleClick}>
+      {connectorIndex === -1 ? (
+        <Link href="https://metamask.io/download">{"Get MetaMask"}</Link>
+      ) : (
+        `Connect MetaMask`
       )}
-    </>
-  ) : null;
+    </Button>
+  );
 };

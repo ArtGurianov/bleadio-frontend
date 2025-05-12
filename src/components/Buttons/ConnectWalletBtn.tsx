052a3de3bd40b81c1e8b15e99ca5076ca005a038
Art <artgurianov@yandex.ru>
2025-05-12 14:08:40 +0300

    deeplink fallback

--- src/components/Buttons/ConnectWalletBtn.tsx
+++ src/components/Buttons/ConnectWalletBtn.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useDeepLink } from "@/lib/hooks/useDeepLink";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useAccount, useConnect } from "wagmi";
 
+const FALLBACK_URL = "https://metamask.io/download";
+
 export const ConnectWalletBtn = () => {
-  const router = useRouter();
   const { isConnected } = useAccount();
   const { connect, connectors } = useConnect();
   const connectorIndex = connectors.findIndex(
     (connector) => connector.name === "MetaMask"
   );
+  const [deeplink, setDeeplink] = useState<string | null>(null);
+  useDeepLink(deeplink, FALLBACK_URL);
 
   if (connectorIndex !== -1) {
     connectors[connectorIndex].getProvider().then((provider) => {
       // @ts-expect-error: wagmi ships provider with unknown type
-      provider.on("display_uri", (uri: string) => router.push(uri));
+      provider.on("display_uri", (deeplink: string) => {
+        setDeeplink(deeplink);
+      });
     });
   }
 
---
+++ src/lib/hooks/useDeepLink.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export const useDeepLink = (url: string | null, fallbackUrl: string) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!url) return;
+    const timeoutId = setTimeout(() => {
+      router.push(fallbackUrl);
+    }, 500);
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        clearTimeout(timeoutId);
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    router.push(url);
+
+    return () => {
+      clearTimeout(timeoutId);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, [url, fallbackUrl]);
+};
